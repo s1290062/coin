c0f83ce4668e3aae2f67b090172a65276569f3f7
Miyazaki Sota <s1290062@u-aizu.ac.jp>
2022-07-14 17:15:52 +0900

    User name added

--- coin.java
+++ coin.java
@@ -1,20 +1,15 @@
 import java.util.*;
+
 class coin{
     public static void main(String[] args){
-	int a,i;
-	Random random =new Random();
-	System.out.println("Tossing a coin...");
-	for(i=1;i<4;i++){
-	    a = random.nextInt(2)%2;
-	    if(a==1){   
-		System.out.println("Round "+i+": Heads");
-	       
-	    }
-	    if(a==0){
-		System.out.println("Round "+i+": Tails");
-	    }
-	}
-	System.out.println("Heads :1, Tails: 2");
+
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Who are you?");
+	System.out.print(">");
+	String name = sc.next();
+	System.out.println("Hello, "+name+"!");
+
 	
     }
 }
