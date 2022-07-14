4b56ea08740178ef495c101434f190b857c6f8d7
Miyazaki Sota <s1290062@u-aizu.ac.jp>
2022-07-14 17:19:34 +0900

    Victory message added

--- coin.java
+++ coin.java
@@ -1,20 +1,23 @@
 import java.util.*;
 class coin{
     public static void main(String[] args){
-	int a,i;
+	int i,a,c=0;
+
 	Random random =new Random();
 	System.out.println("Tossing a coin...");
 	for(i=1;i<4;i++){
 	    a = random.nextInt(2)%2;
 	    if(a==1){   
 		System.out.println("Round "+i+": Heads");
-	       
+		c++;
 	    }
 	    if(a==0){
 		System.out.println("Round "+i+": Tails");
 	    }
 	}
-	System.out.println("Heads :1, Tails: 2");
+	    System.out.println("Heads :1, Tails: 2");
+	    if(c>=2) System.out.println("you won!");
+	    else System.out.println("you lost!");
 	
     }
 }
